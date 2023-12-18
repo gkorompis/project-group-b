ffec297feacde176a787a91471369c5334757a96
aljeazsharon <aljeazsharon@gmail.com>
2023-12-18 23:39:34 +0700

    Update index.js
    feat: Add Product & Transaction module exports

--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -3,6 +3,8 @@ const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 const marketRoute = require('./market.route');
 const storeRoute = require('./store.route');
+const productRoute = require('./product.route');
+const transactionRoute = require('./transaction.route');
 const docsRoute = require('./docs.route');
 const config = require('../../config/config');
 

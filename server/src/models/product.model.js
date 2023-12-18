cb6d179f16fde1aa6ef8ec929a5cd6afefb56530
enkod-id <natanaeltamto@gmail.com>
2023-12-18 20:44:18 +0700

    be

---
+++ server/src/models/product.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const productSchema = mongoose.Schema(
+  {
+    idStore: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+productSchema.plugin(toJSON);
+productSchema.plugin(paginate);
+
+/**
+ * @typedef Product
+ */
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
